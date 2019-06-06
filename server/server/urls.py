15e5f8d46de688354e7caae7683d56b7c583aea6
Anton Petrov <antonpetrov@MacBook-Pro-Anton.local>
2019-06-07 01:25:46 +0700

    Создан проект интернет витрины
    Создано приложение main - шаблоны описаны на django template language;
    Создано приложение products;
    Унифицирован url-диспатчер.

--- server/server/urls.py
+++ server/server/urls.py
@@ -14,16 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-from main.views import (
-    index, contacts, about
-)
+from django.urls import path, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('about/', about),
-    path('contacts/', contacts),
-    path('', index),
+    path('products/', include('products.urls')),
+    path('', include('main.urls')),
 ]
