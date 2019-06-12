8d7c58236b6e95512bdfd03241cdb516a39e6dfa
Gitforfun <ultradey@mail.ru>
2019-06-12 18:00:11 +0300

    Revert "Merged in templates (pull request #1)"
    This reverts commit 93762d13ddb98cb67d0665ef10a48800913be3ab, reversing
    changes made to 44ac9ef9bd93e246bf54a2369856b1c9af854927.

--- server/main/views.py
+++ server/main/views.py
@@ -1,49 +1,14 @@
 from django.shortcuts import render
-from django.template import Template, Context
-from django.utils.html import mark_safe
-from django.template.loader import get_template, render_to_string
-from django.http import HttpResponse
 
 
 # Create your views here.
 def index(request):
-    # CONST = False
-
-    # template = Template('<h1>{{ variable }}</h1>')
-
-    # if CONST:
-    #     context_value = {'variable':'Hello'} 
-    # else:
-    #     context_value = {'variable':'World'}
-
-    # context = Context(context_value)
-
-    # return HttpResponse(
-    #     template.render(context)
-    # )
-    template = get_template('main/index.html')
-    context = {'description': 'Главная страница Интернет-витрины'}
-    return HttpResponse(
-        template.render(context)
-    )
+    return render(request, 'index.html')
 
 
 def about(request):
-    return HttpResponse(
-        render_to_string(
-            'main/about.html',
-            {'description': 'Информационная страница Интернет-витрины'}
-        )
-    )
+    return render(request, 'about.html')
 
 
 def contacts(request):
-    return render(
-        request, 
-        'main/contacts.html',
-        {
-            'contacts': [
-                'Контакт1','Контакт2','Контакт3',
-            ]
-        }
-    )
+    return render(request, 'contacts.html')
